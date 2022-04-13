73539b853c01cd3f8ce57b407399dfd81b1f6862
gyurel <gyurel@yahoo.com>
2022-04-13 02:09:40 +0200

    unit and integrations test started next

--- online_shop/online_shop/online_shop/web/tests.py
+++ online_shop/online_shop/online_shop/web/tests.py
@@ -2,9 +2,9 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 # Create your tests here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
-from online_shop.web.models import Profile, Storage
+from online_shop.web.models import Profile, Storage, Product, Cart, Favorites
 
 UserModel = get_user_model()
 
@@ -20,6 +20,20 @@ class IndexViewTests(TestCase):
         'gender': 'Not specified',
     }
 
+    VALID_PRODUCT_DATA = {
+        'name': 'Product',
+        'product_image': 'image',
+        'category': '----------------',
+    }
+
+    VALID_STORAGE_DATA = {
+        'quantity': 5,
+    }
+
+    VALID_CART_DATA = {
+        'quantity': 1,
+    }
+
     def __create_user(self, **credentials):
         return UserModel.objects.create_user(**credentials)
 
@@ -29,10 +43,30 @@ class IndexViewTests(TestCase):
             **self.VALID_PROFILE_DATA,
             user=user,
         )
-        return (user, profile,)
+        product = Product.objects.create(
+            **self.VALID_PRODUCT_DATA,
+        )
+
+        storage = Storage.objects.create(
+            **self.VALID_STORAGE_DATA,
+            product=product,
+        )
+
+        cart = Cart.objects.create(
+            **self.VALID_CART_DATA,
+            product=product,
+            user=user,
+        )
+
+        favorites = Favorites.objects.create(
+            user=user,
+            product=product,
+        )
+
+        return user, profile, product, storage, cart, favorites
 
     def test_if_user_logged_in_access_granted(self):
-        user, profile = self.__create_valid_user_and_profile()
+        user, profile, product, storage, cart, favorites = self.__create_valid_user_and_profile()
 
         self.client.login(**self.VALID_USER_CREDENTIALS)
 
@@ -42,7 +76,21 @@ class IndexViewTests(TestCase):
         self.assertTemplateUsed(response, 'index.html')
 
     def test_if_gets_the_storage_data_in_context(self):
-        storage = Storage.objects.all()
+        user, profile, product, storage, cart, favorites = self.__create_valid_user_and_profile()
+        expected_storage = Storage.objects.all()
+        expected_products = Product.objects.all()
+
+        self.client.login(**self.VALID_USER_CREDENTIALS)
+
+        response = self.client.get(reverse_lazy('home page'))
+
+        storage = response.context['storage']
+        products = response.context['products_list']
+
+        # Check for actual profiles
+        self.assertEqual(expected_storage, storage)
+        self.assertEqual(expected_products, products)
+
 
     def test_if_redirects_to_login_if_user_not_logged_in(self):
         pass
